0d2c29772fa402df2fdd7e7d855c4f910e417bf2
void <git.email.address@googlemail.com>
2009-11-23 05:39:42 +0100

    Fixed the build script, added static library/include features for modules

--- build.py
+++ build.py
@@ -1,8 +1,15 @@
-import nil.build
+import os
+import nil.build, nil.environment, nil.setup
 
 project = 'theodwalha'
+
+nil.environment.unix_like()
 
 builder = nil.build.builder(project)
 for library in ['boost_system', 'boost_thread', 'boost_filesystem', 'ail', 'z', 'dl']:
 	builder.library(library)
-builder.program()
+
+if builder.program():
+	builder.link_static_library()
+	nil.setup.include(project)
+	nil.setup.library(builder.library)
